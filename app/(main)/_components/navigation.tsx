0dfc68090dd48b3ae9fa0fa8b1bc2d66db10fedb
edwardbudaza <edward.budaza@yahoo.co.za>
2024-05-20 15:54:52 +0200

    search dialog

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -20,6 +20,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import { useSearch } from '@/hooks/use-search';
 
 import UserItem from './user-item';
 import { Item } from './item';
@@ -27,6 +28,7 @@ import { DocumentList } from './document-list';
 import { TrashBox } from './trash-box';
 
 export const Navigation = () => {
+  const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px");
   const create = useMutation(api.documents.create);
@@ -146,7 +148,7 @@ export const Navigation = () => {
             label="Search"
             icon={Search}
             isSearch
-            onClick={() => {}}
+            onClick={search.onOpen}
           />
           <Item 
             label="Settings"
